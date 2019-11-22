565e82a849a411974942f6bdced3cc0620750c8a
theharisshah <shahharis383@gmail.com>
2019-11-22 20:45:21 +0530

    First Commit

---
+++ pyscappe.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome('/Users/harisshah/Downloads/chromedriver')
+
+driver.get('https://www.linkedin.com')
+driver.fullscreen_window()
+driver.implicitly_wait(30)
+email = driver.find_element_by_name('session_key')
+driver.implicitly_wait(30)
+email.send_keys('example@email.com')
+driver.implicitly_wait(30)
+password = driver.find_element_by_name('session_password')
+driver.implicitly_wait(30)
+password.send_keys('Password')
+driver.implicitly_wait(30)
+driver.find_elements_by_class_name('sign-in-form__submit-btn')[0].click()
+# enterButton = driver.find_elements_by_class_name('sign-in-form__submit-btn').click()
+# print(driver.find_elements_by_tag_name('button'))
